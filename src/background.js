82db9b72f0ca954633fd87c5f10c0bb58afc191d
Markus Birth <mbirth@gmail.com>
2013-06-25 09:36:12 +0200

    Changed it to a page_action to only show icon on G+ pages

--- src/background.js
+++ src/background.js
@@ -1,3 +1,11 @@
+chrome.tabs.onUpdated.addListener(
+    function(tabId, changeInfo, tab) {
+        if (tab.url.indexOf('://plus.google.com/') > -1) {
+            chrome.pageAction.show(tabId);
+        }
+    }
+);
+
 chrome.extension.onRequest.addListener(
     function(request, sender, sendResponse) {
         if (request.method == "getBlacklist") {
